c9dfb77d026de1b1f42f04355b34a936faaf71f5
Matthew Teague <matthew.teg99@gmail.com>
2021-06-05 10:23:18 -0400

    Q1 done

---
+++ q1_api.py
@@ -0,0 +1,107 @@
+import numpy as np
+import random
+import math
+from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
+from scipy.optimize import minimize
+from scipy.special import expit
+
+# Distribution parameters
+priors = np.array([0.65, 0.35])
+m01 = np.array([3, 0])
+C01 = np.array([[2, 0], [0, 1]])
+m02 = np.array([0, 3])
+C02 = np.array([[1, 0], [0, 2]])
+m1 = np.array([2, 2])
+C1 = np.array([[1, 0], [0, 1]])
+
+
+def generate_dataset(n):
+    data = np.zeros(shape=(2, n))
+    labels = [0 if random.uniform(0, 1) < 0.65 else 1 for i in range(n)]
+    for i in range(n):
+        if labels[i] == 0:
+            if random.uniform(0, 1) < 0.5:
+                data[:, i] = np.random.multivariate_normal(mean=m01, cov=C01).T
+            else:
+                data[:, i] = np.random.multivariate_normal(mean=m02, cov=C02).T
+        else:
+            data[:, i] = np.random.multivariate_normal(mean=m1, cov=C1).T
+    return data, labels
+
+
+def plot_dataset(data, labels, title):
+    plt.scatter(data[0], data[1], color=['blue' if i == 0 else 'red' for i in labels])
+    plt.title(title)
+    plt.legend(handles=[Line2D([0], [0], marker='o', color='w', label='Class 0', markerfacecolor='b', markersize=10),
+                        Line2D([0], [0], marker='o', color='w', label='Class 1', markerfacecolor='r', markersize=10)])
+    plt.show()
+
+
+def linear_z(x):
+    n = x.shape[1]
+    z = np.zeros(shape=(3, n))
+    for i in range(n):
+        z[0][i] = 1
+        z[1][i] = x[0][i]
+        z[2][i] = x[1][i]
+    return z
+
+
+def quadratic_z(x):
+    n = x.shape[1]
+    z = np.zeros(shape=(6, n))
+    for i in range(n):
+        z[0][i] = 1
+        z[1][i] = x[0][i]
+        z[2][i] = x[1][i]
+        z[3][i] = x[0][i] * x[0][i]
+        z[4][i] = x[0][i] * x[1][i]
+        z[5][i] = x[1][i] * x[1][i]
+    return z
+
+
+def linear_logistic_function(x, w):
+    wTz = np.matmul(w.T, np.array([1, x[0], x[1]]))
+    return expit(wTz)
+
+
+def quadratic_logistic_function(x, w):
+    wTz = np.matmul(w.T, np.array([1, x[0], x[1], x[0] * x[0], x[0] * x[1], x[1] * x[1]]))
+    return expit(wTz)
+
+
+def cost(w, x, l, h):
+    n = x.shape[1]
+    c = 0
+    for i in range(n):
+        c = c + l[i] * np.log(h(x[:, i], w)) + (1 - l[i]) * np.log(1 - h(x[:, i], w))
+    return c * (-1/n)
+
+
+def logistic_regression(train_x, train_l, val_x, val_l, title, quadratic=False):
+    if quadratic:
+        h = quadratic_logistic_function
+        z = quadratic_z
+        w = np.array([0, 0, 0, 0, 0, 0])
+    else:
+        h = linear_logistic_function
+        z = linear_z
+        w = np.array([0, 0, 0])
+
+    sol = minimize(cost, w, args=(train_x, train_l, h))
+    w = sol.x
+    d = np.matmul(w.T, z(val_x))
+    dL0 = d[[i == 0 for i in val_l]]
+    dL1 = d[[i == 1 for i in val_l]]
+    p_error = (np.count_nonzero(dL0 >= 0) + np.count_nonzero(dL1 < 0)) / np.size(d)
+    print(title + ' Pr(error):')
+    print(p_error)
+    L0 = val_x[:, [i == 0 for i in val_l]]
+    L1 = val_x[:, [i == 1 for i in val_l]]
+    colors0 = ['green' if dL0[i] < 0 else 'red' for i in range(len(dL0))]
+    colors1 = ['green' if dL1[i] >= 0 else 'red' for i in range(len(dL1))]
+    plt.scatter(L0[0], L0[1], marker='o', color=colors0)
+    plt.scatter(L1[0], L1[1], marker='^', color=colors1)
+    plt.title(title)
+    plt.show()
---
+++ q1_main.py
@@ -0,0 +1,81 @@
+import numpy as np
+import random
+from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
+from scipy import stats
+from math import isclose
+
+import q1_api as q1
+
+# Generate datasets
+D20_train, L20_train = q1.generate_dataset(20)
+D200_train, L200_train = q1.generate_dataset(200)
+D2000_train, L2000_train = q1.generate_dataset(2000)
+D10k_val, L10k_val = q1.generate_dataset(10000)
+
+# Plot datasets
+# q1.plot_dataset(D20_train, L20_train, '20 Training Samples')
+# q1.plot_dataset(D200_train, L200_train, '200 Training Samples')
+# q1.plot_dataset(D2000_train, L2000_train, '2000 Training Samples')
+q1.plot_dataset(D10k_val, L10k_val, '10k Validation Samples')
+
+# Optimal classifier
+FPR = []
+TPR = []
+gammas = []
+p_error = []
+pxL0 = 0.5*stats.multivariate_normal.pdf(D10k_val.T, q1.m01, q1.C01) + \
+       0.5*stats.multivariate_normal.pdf(D10k_val.T, q1.m02, q1.C02)
+pxL1 = stats.multivariate_normal.pdf(D10k_val.T, q1.m1, q1.C1)
+d = np.divide(pxL1, pxL0)
+dL0 = d[[i == 0 for i in L10k_val]]
+dL1 = d[[i == 1 for i in L10k_val]]
+gamma_min = 0
+gamma_max = np.max(d)
+gamma_inc = 0.1
+gamma = gamma_min
+while gamma < gamma_max:
+    FP = np.count_nonzero(dL0 >= gamma)
+    N = np.size(dL0)
+    FPR.append(FP / N)
+    TP = np.count_nonzero(dL1 >= gamma)
+    P = np.size(dL1)
+    TPR.append(TP / P)
+    FN = np.count_nonzero(dL1 < gamma)
+    p_error.append((FP / N)*q1.priors[0] + (FN / P)*q1.priors[1])
+    gammas.append(gamma)
+    gamma = gamma + gamma_inc
+# plt.plot(FPR, TPR, color='blue')
+gamma = q1.priors[0] / q1.priors[1]
+FP = np.count_nonzero(dL0 >= gamma)
+N = np.size(dL0)
+TP = np.count_nonzero(dL1 >= gamma)
+P = np.size(dL1)
+FN = np.count_nonzero(dL1 < gamma)
+# plt.scatter(FP / N, TP / P, color='green')
+# print('Optimal gamma: ')
+# print(gamma)
+# print('Optimal min error: ')
+# print((FP / N)*q1.priors[0] + (FN / P)*q1.priors[1])
+# plt.title('ERM ROC Curve')
+# plt.legend(handles=[Line2D([0], [0], marker='o', color='w', label='Optimal Gamma', markerfacecolor='g', markersize=10)])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.xlim(0, 1)
+# plt.ylim(0, 1)
+# plt.show()
+L0 = D10k_val[:, [i == 0 for i in L10k_val]]
+L1 = D10k_val[:, [i == 1 for i in L10k_val]]
+# colors0 = ['green' if dL0[i] < gamma else 'red' for i in range(len(dL0))]
+# colors1 = ['green' if dL1[i] >= gamma else 'red' for i in range(len(dL1))]
+# plt.scatter(L0[0], L0[1], marker='o', color=colors0)
+# plt.scatter(L1[0], L1[1], marker='^', color=colors1)
+# plt.show()
+
+q1.logistic_regression(D20_train, L20_train, D10k_val, L10k_val, title='20 Training Samples')
+q1.logistic_regression(D200_train, L200_train, D10k_val, L10k_val, title='200 Training Samples')
+q1.logistic_regression(D2000_train, L2000_train, D10k_val, L10k_val, title='2000 Training Samples')
+
+q1.logistic_regression(D20_train, L20_train, D10k_val, L10k_val, title='20 Training Samples', quadratic=True)
+q1.logistic_regression(D200_train, L200_train, D10k_val, L10k_val, title='200 Training Samples', quadratic=True)
+q1.logistic_regression(D2000_train, L2000_train, D10k_val, L10k_val, title='2000 Training Samples', quadratic=True)
